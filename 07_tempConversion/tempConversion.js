52c5948d1eafa80f7e510cc5a55387faf2d42505
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-06 15:03:53 +0800

    Finish tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let c = (temp - 32) / 1.8;
+  precision = Math.pow(10, 1);
+  c =  Math.ceil(c * precision) / precision;
 
+  return c;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let f = (temp * 1.8) + 32;
+  precision = Math.pow(10, 1);
+  f =  Math.ceil(f * precision) / precision;
 
+  return f;
 };
 
 // Do not edit below this line
