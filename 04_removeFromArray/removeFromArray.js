600a22f48784a5d5cc20275288451da4321804a5
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-06 11:29:23 +0800

    Finish removefromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, removeVars) {
 
+    for(i = 0; i < arguments.length; i++) {
+      if(arguments[0].indexOf(arguments[i]) != -1) {
+        if(i > 0) {
+          arguments[0].splice(arguments[0].indexOf(arguments[i]), 1)
+        }
+      }
+    }
+  
+    return list
 };
 
 // Do not edit below this line
