b5edb91bc4eef11d0ae5334f05c2bcc1215e154a
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-06 10:58:29 +0800

    Finish repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    let holdString = ''
 
+    if(repeats < 0) {
+      holdString = 'ERROR'
+    }
+  
+    for(i = 0; i < repeats; i++) {
+      holdString = holdString + string
+    }
+  
+    return holdString;
 };
 
 // Do not edit below this line
