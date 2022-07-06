0c8c7335616fd811be7fc0d952eae810af0c18fb
Andrei Asi <johnwayneandreiasi@gmail.com>
2022-07-06 14:36:12 +0800

    Finish sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0
+    let first = 0
+    let last = 0
+  
+    if(a > b) {
+      first = b
+      last = a
+    }
+    else {
+      first = a
+      last = b
+    }
+  
+    for(i=first; i<=last; i++) {
+      sum += i
+    }
+  
+    if(typeof a !== "number" || typeof b !== "number") {
+      sum = 'ERROR'
+    }
+  
+    if(sum < 0) {
+      sum = 'ERROR'
+    }
+  
+    //console.log(sum)
+    return sum
 };
 
 // Do not edit below this line
